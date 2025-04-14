f147f33f8c532ebaef3756664abf3720b2fdc095
Gabriel Tomazini <gtomazini@hotmail.com>
2025-04-14 20:13:11 -0300

    fix driver command + cap

--- featrures/environment.py
+++ featrures/environment.py
@@ -1,10 +1,21 @@
+import os
+from support.capabilities import capabilities
+from appium.options.android import UiAutomator2Options
+from appium import webdriver
 '''
     before_scenario() - Antes de começar qualquer cenário ele vai iniciar o servidor do appium
 
     after_scenario() - Após a execução de todos os testes ele vai fechar o servidor do appium
+
+    base: https://github.com/serhatbolsu/appium-python-bdd/blob/master/features/environment.py
 '''
+
 def before_scenario(context, scenario):
-    context.driver.start_driver()
+    options = UiAutomator2Options()
+    options.load_capabilities(capabilities)
+    
+    appium_server_url = 'http://localhost:4723'
+    context.driver = webdriver.Remote(appium_server_url, options=options)
 
 def after_scenario(context, scenario):
-    context.driver.driver_quit()
+    context.driver.quit()
